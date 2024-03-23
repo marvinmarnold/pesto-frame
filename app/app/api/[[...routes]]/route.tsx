d3de1cc3ce7de312ee722253468f4358c4f558ed
Marvin Arnold <marvin@geeky.rocks>
2024-03-23 18:30:46 -0500

    feat: basic state

--- app/app/api/[[...routes]]/route.tsx
+++ app/app/api/[[...routes]]/route.tsx
@@ -4,20 +4,73 @@ import { Button, Frog } from "frog";
 import { handle } from "frog/vercel";
 import nftAbi from "./nftAbi.json";
 
-const app = new Frog({
+type State = {
+	pasta: 'spaghetti' | 'bowtie' | 'fettuccine' | 'penne' | undefined;
+	base: 'basil' | 'beet' | 'carrot' | 'tomato' | undefined;
+}
+
+const app = new Frog<{State: State}>({
 	basePath: "/api",
+	initialState: {
+		pasta: undefined,
+		base: undefined
+	}
 });
+
 
 app.frame("/", (c) => {
 	return c.res({
-		action: "/finish",
-		image: "https://dweb.mypinata.cloud/ipfs/QmSYN7KT847Nado3fxFafYZgG6NXTMZwbaMvU9jhu5nPmJ",
-		imageAspectRatio: "1:1",
-		intents: [<Button.Transaction target="/mint">Mint</Button.Transaction>],
+		action: "/choose-pasta",
+		image: (
+			<div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+			  Select your base:
+			</div>
+		  ),
+		intents: [
+			<Button value="basil">Basil</Button>,
+			<Button value="beet">Beet</Button>
+		],
 	});
 });
 
-app.frame("/finish", (c) => {
+app.frame("/choose-pasta", (c) => {
+	const { buttonValue, deriveState } = c
+	const state = deriveState(previousState => {
+		previousState.base = buttonValue as State['base']
+	})
+
+	return c.res({
+		action: "/pre-mint",
+		image: (
+			<div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+			  Select your pasta (base = {state.base}):
+			</div>
+		  ),
+		intents: [
+			<Button value="spaghetti">Spaghetti</Button>,
+			<Button value="bowtie">Bowtie</Button>
+		],
+	});
+});
+
+app.frame("/pre-mint", (c) => {
+	const { buttonValue, deriveState } = c
+	const state = deriveState(previousState => {
+		previousState.pasta = buttonValue as State['pasta']
+	})
+
+	return c.res({
+		action: "/mint-successful",
+		image: (
+			<div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+			  Mint your {state.base} {state.pasta} pesto NFT:
+			</div>
+		  ),
+		  intents: [<Button.Transaction target="/mint">Mint</Button.Transaction>],
+	});
+});
+
+app.frame("/mint-successful", (c) => {
 	return c.res({
 		image: "https://dweb.mypinata.cloud/ipfs/QmUx3kQH4vR2t7mTmW3jHJgJgJGxjoBsMxt6z1fkZEHyHJ",
 		imageAspectRatio: "1:1",
