c3da29b22be9fa0451ce895d5031528cc1d7afcb
Marvin Arnold <marvin@geeky.rocks>
2024-03-24 03:21:22 -0500

    feat: finished endpoints

--- app/app/api/[[...routes]]/route.tsx
+++ app/app/api/[[...routes]]/route.tsx
@@ -161,7 +161,11 @@ app.frame("/refresh-img", async (c) => {
 	})
 	
 	// marvin: query /api/img/get
-	const {status, openAiUrl} = {status: "ready", openAiUrl: "https://oaidalleapiprodscus.blob.core.windows.net/private/org-QlV7bUj9CtoUf8UgTXPLL1JH/user-6prQk9LVzsOlvxHfrTfKpMQA/img-SbDNFMgGapPu3iBe80vRM3xj.png?st=2024-03-24T03%3A10%3A03Z&se=2024-03-24T05%3A10%3A03Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-03-23T21%3A32%3A19Z&ske=2024-03-24T21%3A32%3A19Z&sks=b&skv=2021-08-06&sig=62ZsP/z%2BcFBSP%2BvQ%2BQb6VhqMPoaI/ZgNvASfFIzavi0%3D"}
+	// const {status, openAiUrl} = {status: "ready", openAiUrl: "https://oaidalleapiprodscus.blob.core.windows.net/private/org-QlV7bUj9CtoUf8UgTXPLL1JH/user-6prQk9LVzsOlvxHfrTfKpMQA/img-SbDNFMgGapPu3iBe80vRM3xj.png?st=2024-03-24T03%3A10%3A03Z&se=2024-03-24T05%3A10%3A03Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-03-23T21%3A32%3A19Z&ske=2024-03-24T21%3A32%3A19Z&sks=b&skv=2021-08-06&sig=62ZsP/z%2BcFBSP%2BvQ%2BQb6VhqMPoaI/ZgNvASfFIzavi0%3D"}
+	const url = getBaseUrl() + "api/image/query-job?jobId=" + state.openAiJobId
+	const response = await fetch(url)
+
+	const { status, openAiUrl } = await response.json()
 
 	if (status === "ready") {
 		// get ipfs uri and gateway url
